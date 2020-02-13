0e43da2eb6a0ef181a479f2270e0ee3841411819
Filip Vanden Eynde <filipve1994@gmail.com>
2020-02-13 10:26:18 +0100

    First commit

---
+++ src/main/java/com/filipve1994/personalexercisespringbootcrudapi/PersonalExerciseSpringbootCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.filipve1994.personalexercisespringbootcrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalExerciseSpringbootCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalExerciseSpringbootCrudApiApplication.class, args);
+	}
+
+}
